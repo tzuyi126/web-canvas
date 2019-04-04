c1355c039df550fc16eeebfc90437df05dcab0dd
106062123 <belle880126@gmail.com>
2019-04-04 19:52:19 +0800

    update 19.52

--- paint.js
+++ paint.js
@@ -1,69 +1,212 @@
 var draw; 
 var m, c; 
-var tmp_paint;
 var bef_paint=[];
 var af_paint=[];
+var tool;
+var color;
+var brush;
+
+//last mouse
+var lastX,lastY;
 
 function init() {
     m = document.getElementById("m"); 
     c = m.getContext("2d");
     brush1.checked = "checked";
     m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");
+    tool = "pen";
+    color = "black";
+    brush = 1;
 }
+
+//mouse
 function md() {
-    c.moveTo(event.offsetX, event.offsetY);
-    draw = true;
-    c.beginPath();
+    bef_paint.push(c.getImageData(0,0,m.width, m.height));
+    switch(tool){
+        case "eraser":
+            c.strokeStyle = "rgb(255,255,255)";
+            c.lineWidth = brush;
+            c.moveTo(event.offsetX, event.offsetY);
+            draw = true;
+            c.beginPath();
+            break;
+        
+        case "rectangle":
+            c.strokeStyle = color;
+            c.lineWidth = brush;
+            c.moveTo(event.offsetX, event.offsetY);
+            lastX = event.offsetX;
+            lastY = event.offsetY;
+            draw = true;
+            break;
+
+        case "text":
+            c.fillStyle = color;
+            c.font = brush + '2' + 'px fantasy';
+            c.fillText(document.getElementById("text_data").value,event.offsetX,event.offsetY);
+            break;
+            
+        default:
+            c.strokeStyle = color;
+            c.lineWidth = brush;
+            c.moveTo(event.offsetX, event.offsetY);
+            lastX = event.offsetX;
+            lastY = event.offsetY;
+            draw = true;
+            c.beginPath();
+            break;
+    }
 }
 function mv() {
     if (draw) {
-        c.lineTo(event.offsetX, event.offsetY);
-        c.stroke();
+        switch(tool){
+            case "pen":
+                c.lineTo(event.offsetX, event.offsetY);
+                c.stroke();
+                break;
+            
+            case "eraser":
+                c.lineTo(event.offsetX, event.offsetY);
+                c.stroke();
+                break;
+
+            case "rectangle":
+                c.putImageData(bef_paint[bef_paint.length-1], 0, 0);
+                c.beginPath(); 
+                c.lineTo(event.offsetX, lastY);
+                c.lineTo(event.offsetX, event.offsetY);
+                c.lineTo(lastX, event.offsetY);
+                c.lineTo(lastX, lastY);
+                c.closePath();
+                c.stroke();  
+                break;
+
+            case "circle":
+                c.putImageData(bef_paint[bef_paint.length-1], 0, 0);
+                c.beginPath();
+                c.arc(lastX, lastY, Math.sqrt((lastX-event.offsetX)*(lastX-event.offsetX) + (lastY-event.offsetY)*(lastY-event.offsetY)), 0, 2 * Math.PI);
+                c.closePath();
+                c.stroke();  
+                break;
+
+            case "triangle":
+                c.putImageData(bef_paint[bef_paint.length-1], 0, 0);
+                c.beginPath();  
+                c.lineTo(event.offsetX, event.offsetY);
+                c.lineTo(2*lastX-event.offsetX, event.offsetY);
+                c.lineTo(lastX, lastY);
+                c.closePath();
+                c.stroke(); 
+                break;
+
+            case "line":
+                c.putImageData(bef_paint[bef_paint.length-1], 0, 0);
+                c.beginPath();  
+                c.lineTo(event.offsetX, event.offsetY);
+                c.lineTo(lastX, lastY);
+                c.closePath();
+                c.stroke();  
+                break;
+
+            default:
+                c.lineTo(event.offsetX, event.offsetY);
+                c.stroke();
+                break;
+        }
     }
 }
 function mup() {
-    if(draw){
+    if(draw && event.offsetX>=0 && event.offsetX<=m.width && event.offsetY>=0 && event.offsetY<=m.height){
         draw = false;
         c.closePath();
-        bef_paint.push(c.getImageData(0,0,m.width, m.height));
     }
 }
 
 //undo redo
 function undo(){
-    bef_paint.push(c.getImageData(0,0,m.width, m.height));
-    c.putImageData(af_paint[af_paint.length-1],0,0);
-    af_paint.pop();
-    
+    if(bef_paint.length > 0){
+        af_paint.push(c.getImageData(0,0,m.width, m.height));
+        c.putImageData(bef_paint[bef_paint.length-1], 0, 0);
+        bef_paint.pop();
+    }
 }
 function redo(){
-    af_paint.push(c.getImageData(0,0,m.width, m.height));
-    c.putImageData(bef_paint[bef_paint.length-1], 0, 0);
-    bef_paint.pop();
-
+    if(af_paint.length > 0){
+        bef_paint.push(c.getImageData(0,0,m.width, m.height));
+        c.putImageData(af_paint[af_paint.length-1],0,0);
+        af_paint.pop();
+    }
 }
 
-function color1() { c.strokeStyle = "black"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color2() { c.strokeStyle = "red";m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color3() { c.strokeStyle = "orange"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color4() { c.strokeStyle = "yellow"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color5() { c.strokeStyle = "green"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color6() { c.strokeStyle = "blue"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color7() { c.strokeStyle = "purple"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
-function color8() { c.strokeStyle = "brown"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+//upload pic
+function uploading(){
+    var upload = document.getElementById("upload");
+    upload.addEventListener("change",uploadImage,false);
 
-function brush1() { c.lineWidth = 1; }
-function brush2() { c.lineWidth = 3; }
-function brush3() { c.lineWidth = 5; }
-function brush4() { c.lineWidth = 10; }
-function brush5() { c.lineWidth = 20; }
+    function uploadImage(e){
+        var reader = new FileReader();
 
-function Button1_onclick() { 
+        reader.onload = function(event){
+            var img = new Image();
+            img.onload = function () {
+                c.drawImage(img,0,0,img.width,img.height);
+            }
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);
+	}
+}
+
+function color1() { color = "black"; }
+function color2() { color = "red";}
+function color3() { color = "orange";}
+function color4() { color = "yellow"; }
+function color5() { color = "green"; }
+function color6() { color = "blue"; }
+function color7() { color = "purple";}
+function color8() { color = "brown"; }
+
+function brush1() { brush = 1; }
+function brush2() { brush = 3; }
+function brush3() { brush = 5; }
+function brush4() { brush = 10; }
+function brush5() { brush = 20; }
+
+function reset() { 
     c.clearRect(0,0,m.width, m.height); 
-    af.push(c.getImageData(0,0,m.width, m.height));
+    bef_paint.push(c.getImageData(0,0,m.width, m.height));
 }
-function Button2_onclick() { 
-    c.strokeStyle = "rgb(255,255,255)"; 
+
+// tool
+function pen() { 
+    tool = "pen";
+    m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");
+}
+
+function eraser() { 
+    tool = "eraser";
     m.setAttribute("style", "cursor:url(image/eraser.cur),default;");
 }
 
+function rectangle() { 
+    tool = "rectangle";
+    m.style.cursor = "crosshair";
+}
+function circle() { 
+    tool = "circle";
+    m.style.cursor = "crosshair";
+}
+function triangle() { 
+    tool = "triangle";
+    m.style.cursor = "crosshair";
+}
+function line() { 
+    tool = "line";
+    m.style.cursor = "crosshair";
+}
+
+// text
+function text(){
+    m.style.cursor = "text";
+    tool = "text";
+}
