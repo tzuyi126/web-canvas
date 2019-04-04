9ed24b73be4d8bb9b205e34c33f5f9d84f925c92
106062123 <belle880126@gmail.com>
2019-04-04 11:06:00 +0800

    update 11.05

--- paint.js
+++ paint.js
@@ -1,33 +1,57 @@
-var draw; //是否在繪圖狀態
-var m, c; //繪圖物件
+var draw; 
+var m, c; 
+var tmp_paint;
+var bef_paint=[];
+var af_paint=[];
+
 function init() {
-    m = document.getElementById("m"); //取得畫布物件參考
-    c = m.getContext("2d"); //建立2d繪圖物件
+    m = document.getElementById("m"); 
+    c = m.getContext("2d");
     brush1.checked = "checked";
+    m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");
 }
 function md() {
-    c.moveTo(event.offsetX, event.offsetY); //起點
-    draw = true; //進入繪圖模式
-    c.beginPath(); //本次繪圖筆畫開始
+    c.moveTo(event.offsetX, event.offsetY);
+    draw = true;
+    c.beginPath();
 }
 function mv() {
     if (draw) {
-        c.lineTo(event.offsetX, event.offsetY); //下一點
-        c.stroke(); //繪圖
+        c.lineTo(event.offsetX, event.offsetY);
+        c.stroke();
     }
 }
 function mup() {
-    draw = false; //離開繪圖模式
-    c.closePath(); //繪圖筆畫結束
+    if(draw){
+        draw = false;
+        c.closePath();
+        bef_paint.push(c.getImageData(0,0,m.width, m.height));
+    }
+    save();
 }
-function color1() { c.strokeStyle = "black"; m.style.cursor = "url(pencil.png),auto"; }
-function color2() { c.strokeStyle = "red"; m.style.cursor = "url(pencil.png),auto";}
-function color3() { c.strokeStyle = "orange"; }
-function color4() { c.strokeStyle = "yellow"; }
-function color5() { c.strokeStyle = "green"; }
-function color6() { c.strokeStyle = "blue"; }
-function color7() { c.strokeStyle = "purple"; }
-function color8() { c.strokeStyle = "brown"; }
+
+//undo redo
+function undo(){
+    bef_paint.push(c.getImageData(0,0,m.width, m.height));
+    c.putImageData(af[af.length-1],0,0);
+    af.pop();
+    
+}
+function redo(){
+    af.push(c.getImageData(0,0,m.width, m.height));
+    c.putImageData(bef[bef.length-1], 0, 0);
+    bef.pop();
+
+}
+
+function color1() { c.strokeStyle = "black"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color2() { c.strokeStyle = "red";m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color3() { c.strokeStyle = "orange"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color4() { c.strokeStyle = "yellow"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color5() { c.strokeStyle = "green"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color6() { c.strokeStyle = "blue"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color7() { c.strokeStyle = "purple"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
+function color8() { c.strokeStyle = "brown"; m.setAttribute("style", "cursor:url(image/featherpenpointer.cur),default;");}
 
 function brush1() { c.lineWidth = 1; }
 function brush2() { c.lineWidth = 3; }
@@ -35,5 +59,12 @@ function brush3() { c.lineWidth = 5; }
 function brush4() { c.lineWidth = 10; }
 function brush5() { c.lineWidth = 20; }
 
-function Button1_onclick() { location.reload(); }
-function Button2_onclick() { c.strokeStyle = "rgb(255,255,255)"; }
+function Button1_onclick() { 
+    c.clearRect(0,0,m.width, m.height); 
+    af.push(c.getImageData(0,0,m.width, m.height));
+}
+function Button2_onclick() { 
+    c.strokeStyle = "rgb(255,255,255)"; 
+    m.setAttribute("style", "cursor:url(image/eraser.cur),default;");
+}
+
